bb071ef2ec46cc2ea02266c6c888d86391ccade3
alison-at <alisonateske@gmail.com>
2026-02-03 16:27:37 -0500

    updated readme

--- hw1/TestHashes.java
+++ hw1/TestHashes.java
@@ -4,15 +4,19 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
+import java.util.Random;
 import java.lang.Object;
-import org.apache.commons.lang3.RandomStringUtils;
+
+
 //should turn this to junit
 public class TestHashes {
     public static void main(String[] args) {
-        testOne();
-        testTwo();
-        testThree();
+        //testOne();
+        //testTwo();
+        findCollision();
+        //testThree();
     }
 
     public static void printBytes(byte[] message) {
@@ -35,24 +39,54 @@ public class TestHashes {
 
     }
 
-    //TODO
-    public static void findCollisionSha256() {
-        //make a random number generator for ascii chars
-        for (int i - )
-        HashMap<String, String> map = new HashMap<>();
-        RandomStringUtils stringUtils = new RandomStringUtils();
-        String asciiString = stringUtils.nextAscii(10);
-        
-        byte[] asciiBytes = asciiString.getBytes(StandardCharsets.UTF_8);
-
-        byte[] hashedBytes = Utils.sha256(asciiBytes);
-        String hashedString = new String(hashedBytes, StandardCharsets.UTF_8);
-        if (map.get(hashedString) == null) {
-            map.put(hashedString, asciiString);
-        } else {
-            System.out.println()
-            return hashedString;
+    public static String generateString() {
+        int asciiStart = 32;
+        int asciiEnd = 126; 
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomChar = asciiStart + (int) (random.nextFloat() * (asciiEnd - asciiStart + 1));
+            buffer.append((char) randomChar);
         }
+        String generatedString = buffer.toString();
+        return generatedString;
+    }
+
+    //TODO
+    public static void findCollision() {
+        //make a random number generator for ascii chars
+        long start = System.currentTimeMillis();
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; (System.currentTimeMillis() - start) < 1800000 ; i++) {
+            //RandomStringUtils stringUtils = new RandomStringUtils();
+            String asciiString = generateString();
+            
+            byte[] asciiBytes = asciiString.getBytes();
+            try {
+                byte[] hashedBytes = Part1.computeDigest(asciiBytes, 3);
+                String hashedString = Base64.getEncoder().encodeToString(hashedBytes);
+                if (map.get(hashedString) == null ) {
+                    map.put(hashedString, asciiString);
+                } else {
+                    if (map.get(hashedString).equals(asciiString)) {
+                        continue;
+                    }
+                    long finish = System.currentTimeMillis();
+                    long timeElapsed = finish - start;
+                    System.out.printf("Collision of %s and %s at %s with time of %d on iteration %d\n", map.get(hashedString), asciiString, hashedString, timeElapsed, i );
+                    return;
+                }
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+
+            if (i%100000 == 0) {
+                System.out.println(i+ "th iteration");
+            }
+        }
+       
+        
 
         
         //add chars to an array, concatenate to string, transform to bytes
